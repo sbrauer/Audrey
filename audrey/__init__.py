547f9076f17e0f3e839c283d8ddac0a0bdc49ec4
Sam Brauer <sam.brauer@gmail.com>
2012-11-11 12:27:47 -0500

    improve/refactor startup

--- audrey/__init__.py
+++ audrey/__init__.py
@@ -1,13 +1,12 @@
 from pyramid.config import Configurator
 from pyramid.settings import aslist
-from audrey.resources import root_factory, root
 import pymongo
 import pyes
+from audrey.resources import root_factory, root
 
-def main(global_config, **settings):
+def audrey_main(root_factory, root_cls, global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    
     # Handle custom settings that require type conversion.
     mongo_uri = aslist(settings['mongo_uri'])
     settings['mongo_uri'] = mongo_uri
@@ -15,6 +14,9 @@ def main(global_config, **settings):
     settings['elastic_uri'] = elastic_uri
     elastic_timeout = float(settings.get('elastic_timeout', '5.0'))
     settings['elastic_timeout'] = elastic_timeout
+    # If "elastic_name" setting not found, fallback to "mongo_name".
+    if 'elastic_name' not in settings:
+        settings['elastic_name'] = settings['mongo_name']
 
     # Standard Pyramid ZCML configuration.
     config = Configurator(root_factory=root_factory, settings=settings)
@@ -25,27 +27,30 @@ def main(global_config, **settings):
     # Do initialization based on custom settings.
     mongo_conn = pymongo.Connection(mongo_uri, tz_aware=True)
     config.registry.settings['mongo_conn'] = mongo_conn
-    ensure_mongo_indexes(mongo_conn, settings['mongo_name'])
+    ensure_mongo_indexes(mongo_conn, settings['mongo_name'], root_cls)
 
     # Not all projects will use Elastic.
     elastic_conn = None
     if elastic_uri:
         elastic_conn = pyes.ES(elastic_uri, timeout=elastic_timeout)
-        ensure_elastic_index(elastic_conn, settings['elastic_name'])
+        ensure_elastic_index(elastic_conn, settings['elastic_name'], root_cls)
     config.registry.settings['elastic_conn'] = elastic_conn
 
     # Finally, return a wsgi app.
     return config.make_wsgi_app()
 
-def ensure_mongo_indexes(conn, db_name):
+def ensure_mongo_indexes(conn, db_name, root_cls):
     db = conn[db_name]
-    for coll_cls in root.Root.get_collection_classes():
-        mongo_coll = db[cls._collection_name]
-        for (key_or_list, kwargs) in cls.get_mongo_indexes():
+    for coll_cls in root_cls.get_collection_classes():
+        mongo_coll = db[coll_cls._collection_name]
+        for (key_or_list, kwargs) in coll_cls.get_mongo_indexes():
             mongo_coll.ensure_index(key_or_list, **kwargs)
 
-def ensure_elastic_index(conn, idx_name):
+def ensure_elastic_index(conn, idx_name, root_cls):
     conn.create_index_if_missing(idx_name)
-    for coll_cls in root.Root.get_collection_classes():
+    for coll_cls in root_cls.get_collection_classes():
         if coll_cls._use_elastic:
-            conn.put_mapping(cls._collection_name(), {'properties':cls.get_elastic_mapping()}, [idx_name])
+            conn.put_mapping(coll_cls._collection_name, {'properties':coll_cls.get_elastic_mapping()}, [idx_name])
+
+def main(global_config, **settings):
+    return audrey_main(root_factory, root.Root, global_config, **settings)
